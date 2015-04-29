de9a76faf5efee56766cd9f39cbb23a7bd5ccb2c
Eric Daams <eric@ericnicolaas.com>
2015-04-29 17:45:02 +0930

    Form styling fixes. Better handling percentage completed.

--- campaign-barometer.php
+++ campaign-barometer.php
@@ -17,6 +17,6 @@ $campaign = charitable_get_current_campaign();
 	data-progress-stroke="<?php echo get_theme_mod( 'body_text', '#7D6E63' ) ?>"
 	>
 	<span>
-		<?php printf( _x( "%s Funded", 'x percent funded', 'benny' ), '<span>' . $campaign->get_percent_donated_raw() . '<sup>%</sup></span>' ) ?>
+		<?php printf( _x( "%s Funded", 'x percent funded', 'benny' ), '<span>' . number_format( $campaign->get_percent_donated_raw(), 0 ) . '<sup>%</sup></span>' ) ?>
 	</span>
 </div>		
