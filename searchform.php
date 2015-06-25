4d6ff87ac9ca2d42a694497af26c4457bf17f9c9
Eric Daams <eric@164a.com>
2015-06-26 00:52:33 +0930

    Update to palettes.

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
-<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ) ?>">
+wp <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ) ?>">
 	<label>
 		<span class="screen-reader-text"><?php _ex( 'Search for:', 'search label', 'benny' ) ?></span>
 		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'search placeholder', 'benny' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'search label' ) ?>" />
