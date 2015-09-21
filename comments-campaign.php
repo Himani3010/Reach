492bddc871a7587876cef2c4749a7e95fe812fa4
Eric Daams <eric@164a.com>
2015-09-21 22:54:03 +0930

    New version (0.9.22). Use default comment display for campaign comments

--- comments-campaign.php
+++ comments-campaign.php
@@ -66,7 +66,7 @@ if ( ! comments_open() ) return; ?>
 						 * define textural_comment() and that will be used instead.
 						 * See textural_comment() in reach/functions.php for more.
 						 */
-						wp_list_comments( array( 'callback' => 'reach_campaign_comment' ) );
+						wp_list_comments( array( 'callback' => 'reach_comment' ) );
 					?>
 				</ul>
 
