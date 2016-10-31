c70403b7b6801d251bfd670c889043867839cd3e
Thiago Censi <tacensi@gmail.com>
2016-10-30 22:41:56 -0200

    Fix error 404 page banner length

--- 404.php
+++ 404.php
@@ -6,13 +6,20 @@
  */
 
 get_header();
+?>
+<main id="main" class="site-main site-content cf error-404">
+	<div class="layout-wrapper">
+		<div id="primary" class="content-area no-sidebar">
 
-get_template_part( 'partials/banner' ); ?>
+			<?php get_template_part( 'partials/banner' ); ?>
 
-<div class="site-main error-404 not-found entry" role="main">
-	<h2><?php _e( 'Sorry, but you\'ve hit a dead end.', 'reach' ) ?></h2>
+			<div class="site-main error-404 not-found entry" role="main">
+				<h2><?php _e( 'Sorry, but you\'ve hit a dead end.', 'reach' ) ?></h2>
 
-	<p><a href="<?php echo site_url() ?>"><?php _e( 'Go home', 'reach' ) ?></a></p>
-</div><!-- .site-main -->
+				<p><a href="<?php echo site_url() ?>"><?php _e( 'Go home', 'reach' ) ?></a></p>
+			</div><!-- .site-main -->
 
+		</div><!-- #primary -->
+	</div><!-- .layout-wrapper -->
+</main><!-- #main -->
 <?php get_footer();
